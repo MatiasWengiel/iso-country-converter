389c92e4deb8efb5970bc8afa1444cd5ce257e82
Matias Wengiel <matiaswengiel@gmail.com>
2024-03-19 00:17:45 -0500

    Add more test scenarios

--- test/converters/alpha-3-to-alpha-2.test.ts
+++ test/converters/alpha-3-to-alpha-2.test.ts
@@ -6,4 +6,18 @@ describe("alpha3ToAlpha2", () => {
 
     expect(converted).toBe("AR");
   });
+
+  test("returns undefined for an unknown country code", () => {
+    const converted = alpha3ToAlpha2("XYZ");
+
+    expect(converted).toBeUndefined();
+  });
+
+  test("returns undefined if the passed string is not three characters in length", () => {
+    const shortConverted = alpha3ToAlpha2("A");
+    const longConverted = alpha3ToAlpha2("WXYZ");
+
+    expect(shortConverted).toBeUndefined();
+    expect(longConverted).toBeUndefined();
+  });
 });
